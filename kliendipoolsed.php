ddd5d2f3ad4c323cbf447374dd09ed6c2eaaa432
Rasmus Pind <rasmus.pind@khk.ee>
2015-09-22 10:20:18 +0300

    3. Tere maailm link {Delivers #103878614}

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,6 +5,7 @@
     <title>RasmusPind PseudoProject</title>
 </head>
 <body>
+<!-- 2. Tere maailm button algus -->
 <button onclick="myFunction()">Tere maailm</button>
 
 <script>
@@ -12,6 +13,19 @@
         alert("tere maailm");
     }
 </script>
+<!-- 2. Tere maailm button lõpp -->
+
+<!-- 3.  Link tekstiga tere maailm, millele klikkides kuvab brauser teadet tere maailm ja siis navigeerib aadressile www.khk.ee -->
+<script type="text/javascript">
+    function AlertIt() {
+        var answer = confirm ("tere maailm");
+        if (answer)
+            window.location="http://www.khk.ee";
+    }
+</script>
+
+<a href="javascript:AlertIt();">tere maailm</a>
+<!-- 3.  Link tekstiga tere maailm, lõpp -->
 
 </body>
 </html>
